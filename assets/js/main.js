e6e34eb1a6ca38b2e896e91516191072d71b85d5
Julia Luo <juliazhiyuan.luo@gmail.com>
2018-06-26 21:11:00 -0700

    update website

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,134 +4,34 @@
 
 $(document).ready(function() {
 
-    //check window size when document loads
-    if($(window).width() <= 600) {
-        $("#profile-pic").attr("src", "assets/images/mdb_cropped.png");
-    }
-    else {
-        $("#profile-pic").attr("src", "assets/images/mdb.png");
-    }
+    setSidebarWidth();
+    setProjectContentWidth();
 
-    //colour changes on home page based on cursor position
-    /*$(document).mousemove(function(e){
-        var $width = ($(document).width())/255;
-        var $height = ($(document).height())/255;
-        var $pageX = parseInt(e.pageX / $width, 10);
-        var $pageY = parseInt(e.pageY / $height, 10);
-        $('#home-header').css('color', 'rgb('+(255-$pageX)+', 0,'+(255-$pageY)+')');
-        $('.home-container').css('color', 'rgb('+$pageX+','+$pageY+', 255)');
-    });*/
-
-    //smooth scrolling
-    $("a").on('click', function(event) {
-
-        if (this.hash !== "") {
-            event.preventDefault();
-
-            var hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function(){
-
-                window.location.hash = hash;
-            });
-        }
-    });
-
-
-    //check window size again whenever window is resized
     window.onresize = function () {
-        if($(window).width() <= 600) {
-            $("#profile-pic").attr("src", "assets/images/mdb_cropped.png");
-            
-        }
-        else {
-            $("#profile-pic").attr("src", "assets/images/mdb.png");
-        }
-    }
+        setSidebarWidth();
+        setProjectContentWidth();
+    };
 
-    var previous_id = 'home-nav';
-    var unselected_color = 'rgba(0, 0, 0, 0)';
-    var selected_color = 'rgba(0, 0, 0, 0.2)';
-
-    $('#home').waypoint(function(direction) {
-        if (direction == 'down') {
-            $('#' + previous_id).css('background-color', unselected_color);
-            $('#home-nav').css('background-color', selected_color);
-            previous_id = 'home-nav';
-        }
-    }, {
-        offset: '1%'
-    });
-
-    $('#home').waypoint(function(direction) {
-        if (direction == 'up') {
-            $('#' + previous_id).css('background-color', unselected_color);
-            $('#home-nav').css('background-color', selected_color); //rgb(181, 170, 255)
-            previous_id = 'home-nav';
-        }
-    }, {
-        offset: '-1%'
-    });
-
-    $('#about').waypoint(function(direction) {
-        if (direction == 'down') {
-            $('#' + previous_id).css('background-color', unselected_color);
-            $('#about-nav').css('background-color', selected_color);
-            previous_id = 'about-nav';
-        }
-    }, {
-        offset: '1%'
-    });
-
-    $('#about').waypoint(function(direction) {
-        if (direction == 'up') {
-            $('#' + previous_id).css('background-color', unselected_color);
-            $('#about-nav').css('background-color', selected_color);
-            previous_id = 'about-nav';
-        }
-    }, {
-        offset: '-1%'
-    });
-
-    $('#projects').waypoint(function(direction) {
-        if (direction == 'down') {
-            $('#' + previous_id).css('background-color', unselected_color);
-            $('#projects-nav').css('background-color', selected_color);
-            previous_id = 'projects-nav';
-        }
-    }, {
-            offset: '1%'
-    });
-
-    $('#projects').waypoint(function(direction) {
-        if (direction == 'up') {
-            $('#' + previous_id).css('background-color', unselected_color);
-            $('#projects-nav').css('background-color', selected_color);
-            previous_id = 'projects-nav';
-        }
-    }, {
-        offset: '-1%'
-    });
-
-    $('#contact').waypoint(function(direction) {
-        if (direction == 'down') {
-            $('#' + previous_id).css('background-color', unselected_color);
-            $('#contact-nav').css('background-color', selected_color);
-            previous_id = 'contact-nav';
-        }
-    }, {
-        offset: '1%'
-    });
-
-    $('#contact').waypoint(function(direction) {
-        if (direction == 'up') {
-            $('#' + previous_id).css('background-color', unselected_color);
-            $('#contact-nav').css('background-color', selected_color);
-            previous_id = 'contact-nav';
-        }
-    }, {
-        offset: '-1%'
+    ['#about', '#home', '#projects'].forEach(function (page) {
+        $(page + "-nav").click(function(e) {
+            $.smoothScroll({
+                scrollElement: $('#content'),
+                scrollTarget: page,
+                speed: 1000,
+            });
+            return false;
+        });
     });
 });
+
+function setSidebarWidth() {
+    var contentWidth = $("#content").width();
+    var contentHeight = $("#content").height();
+    $("ul").width(contentWidth * 0.2);
+    $("ul").height(contentHeight);
+}
+
+function setProjectContentWidth() {
+    projectWidth = $(".project-content").width();
+    $(".projects-container").css("grid-auto-rows", projectWidth);
+}
